84d488fce68a7b5a8253a886619ae2b2b78017f6
Karen Gomes <karen.nayara@audora.com.br>
2018-11-20 12:55:34 -0300

    items creation and prisma yoga flow part 1

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,20 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    //create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    });
   }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   //create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
